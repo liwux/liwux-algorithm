b10a4ea1707312ce19ae797155151006bbe3ee77
liwux <xix0.618@gmail.com>
2022-09-03 17:09:06 +0800

    局部最小

--- class03/src/main/java/Code02.java
+++ class03/src/main/java/Code02.java
@@ -1,3 +1,22 @@
 public class Code02 {
 
+    //arr是有序的，>=num最左的位置
+    public static int mostLeftNolessNumIndex(int[] arr,int num){
+        if (arr==null||arr.length==0){
+            return -1;
+        }
+        int L=0;
+        int R=arr.length-1;
+        int ans = -1;
+        while (L<=R){
+            int mid = (L+R)/2;
+            if (arr[mid]>num){
+                 ans=mid;
+                 R = mid-1;
+            }else {
+                L = mid+1;
+            }
+        }
+        return ans;
+    }
 }
